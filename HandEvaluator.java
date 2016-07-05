90c1deabd3dd336539c669c18fceef1e85d83b5a
Alan Tan <alan_tan_95@yahoo.com>
2016-07-05 14:10:51 -0700

    initial commit

---
+++ HandEvaluator.java
@@ -0,0 +1,15 @@
+/*
+	Alan Tan
+	7.5.16
+
+	This class evaluates a 5 card poker hand
+*/
+
+public class HandEvaluator {
+	public PlayingCard[] hand;
+
+	public static HandType evaluate(PlayingCard[] hand) {
+		this.hand = new PlayingCard[hand.length];
+		
+	}
+}
