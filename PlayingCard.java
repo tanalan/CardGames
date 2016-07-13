2ec36b0706944e2d6412e5055c51e0c03e56247b
Alan Tan <alan_tan_95@yahoo.com>
2016-07-12 23:46:35 -0700

    Unknown change

--- PlayingCard.java
+++ PlayingCard.java
@@ -8,9 +8,9 @@
 
 public class PlayingCard {
 	public enum Rank {
-		ACE("One"), DEUCE("Two"), THREE("Three"), FOUR("Four"), FIVE("Five"),
-        SIX("Six"), SEVEN("Seven"), EIGHT("Eight"), NINE("Nine"), TEN("Ten"),
-        JACK("Jack"), QUEEN("Queen"), KING("King")
+		ACE("1"), DEUCE("2"), THREE("3"), FOUR("4"), FIVE("5"),
+        SIX("6"), SEVEN("7"), EIGHT("8"), NINE("9"), TEN("10"),
+        JACK("J"), QUEEN("Q"), KING("K")
 	}
 	public enum Suit {
 		CLUBS, DIAMONDS, HEARTS, SPADES
