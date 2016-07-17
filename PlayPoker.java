1944f80411816d68209c0f36a9dda4c55dec16eb
Alan Tan <alan_tan_95@yahoo.com>
2016-07-17 01:27:38 -0700

    added a small simulation

--- PlayPoker.java
+++ PlayPoker.java
@@ -1,13 +1,34 @@
-/*
-	Alan Tan
-	7.2.16
+import java.util.Arrays;
 
-	This class will simulate a Poker game
+/*
+    Alan Tan
+    7.2.16
+
+    This class will simulate a Poker game
 */
 
 public class PlayPoker {
-	public static void main(String[] args) {
-	    PlayingDeck deck = new PlayingDeck();
-	
-	}
+    public static void main(String[] args) throws Exception {
+        PlayingDeck deck = new PlayingDeck();
+        //deck.shuffle();
+        PokerPlayer Alan = new PokerPlayer(500);
+        Alan.addCard(deck.draw());
+        Alan.addCard(deck.draw());
+        System.out.println("Alan's cards are " + Alan);
+        System.out.println("Cards up to river: ");
+        System.out.println(deck.draw() + ", " + deck.draw() +
+                            ", " + deck.draw() + ", " + deck.draw() + ", " + 
+                            deck.draw());
+        Alan.addCard(deck.draw());
+        Alan.addCard(deck.draw());
+        Alan.addCard(deck.draw());
+        PlayingCard[] myDeck = new PlayingCard[5];
+        for (int i = 0; i < 5; i++) {
+            myDeck[i] = Alan.hand.get(i);
+        }
+        //System.out.println(Arrays.toString(myDeck));
+        System.out.println("Alan's best hand: " + HandEvaluator.evaluate(myDeck));
+        
+    }
 }
+
