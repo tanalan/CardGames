39740d15d61c7409a19ed15c11a30805238d643b
Alan Tan <alan_tan_95@yahoo.com>
2016-07-25 01:02:50 -0700

    Changed fields

--- PokerPlayer.java
+++ PokerPlayer.java
@@ -7,8 +7,9 @@
 
 public class PokerPlayer {
 
-	private List<PlayingCard> hand;
+	private PlayingCard[] hand;
 	private int cash;
+    private int position; // Shows where to put a new card
 
 	// Constructs a PokerPlayer with the given cash amount
 	// Usually default 500
@@ -17,16 +18,33 @@ public class PokerPlayer {
 		if (cash < 0)
 			throw new IllegalArgumentException();
 		this.cash = cash;
-		this.hand = new ArrayList<PlayingCard>();
+		this.hand = new PlayingCard[7];
 	}
 
 	// Adds the card to the player's hand
 	public void addCard(PlayingCard card) {
-		this.hand.add(card);
+		this.hand[position % 7] = card;
+        this.position++;
 	}
 
 	// Starts a new hand
 	public void clearHand() {
-		this.hand.clear();
+		this.position = 0;
 	}
+
+    // Returns a String representation of the cards
+    // that the player has
+    public String toString() {
+        if (this.position == 0) {
+            return "[]";
+        } else {
+            String result = "[";
+            result += this.hand[0];
+            for (int i = 1; i < this.position % 7; i++) {
+                result += ", " + this.hand[i];
+            }   
+            return result;
+        }
+    
+    }
 }
